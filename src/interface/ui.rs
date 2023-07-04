0f0758debfeffdb6e3946b02261f2631b2263f3a
Amar1729 <amar.paul16@gmail.com>
2023-07-04 03:28:11 -0400

    modularize UI in prep for more complex layouts

--- src/interface/ui.rs
+++ src/interface/ui.rs
@@ -1,6 +1,6 @@
 use ratatui::{
     backend::Backend,
-    layout::{Constraint, Direction, Layout},
+    layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     text::{Line, Span, Text},
     widgets::{Block, Borders, Cell, Paragraph, Row, Scrollbar, ScrollbarOrientation, Table},
@@ -9,39 +9,19 @@ use ratatui::{
 
 use crate::interface::app::{App, InputMode};
 
-/// Renders the user interface widgets.
-pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
-    let outer = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([Constraint::Length(1), Constraint::Min(0)].as_ref())
-        .split(frame.size());
-
+/// Render text input widget
+fn render_input_area<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>, area: Rect) {
     let chunks = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints([Constraint::Min(0), Constraint::Length(1)].as_ref())
-        .split(outer[1]);
-
-    let rows = app.items.iter().map(|show| {
-        Row::new(vec![
-            Cell::from(show.imdb_id.as_str()),
-            Cell::from(show.original_title.clone()),
-            Cell::from(show.release_year.unwrap().to_string()),
-        ])
-    });
-
-    let chunks_textinput = Layout::default()
-        .direction(Direction::Horizontal)
         .constraints([Constraint::Length(10), Constraint::Min(1)].as_ref())
-        .split(outer[0]);
+        .split(area);
 
-    // placeholder for now:
-    // this will be text input for searching for shows
     let (msg, style) = match app.mode {
         InputMode::Normal => (vec![Span::raw("Search ")], Style::default()),
         InputMode::Editing => (
             vec![Span::styled(
                 "Search > ",
-                Style::default().add_modifier(Modifier::BOLD),
+                Style::default().add_modifier(Modifier::REVERSED),
             )],
             Style::default(),
         ),
@@ -51,29 +31,42 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
     text.patch_style(style);
     let help_msg = Paragraph::new(text);
 
-    frame.render_widget(help_msg, chunks_textinput[0]);
+    frame.render_widget(help_msg, chunks[0]);
 
-    {
-        let width = chunks_textinput[1].width.max(3) - 3;
-        let scroll = app.input.visual_scroll(width as usize);
-        let input = Paragraph::new(app.input.value())
-            .style(match app.mode {
-                InputMode::Normal => Style::default(),
-                InputMode::Editing => Style::default().fg(Color::Yellow),
-            })
-            .scroll((0, scroll as u16))
-            .block(Block::default().borders(Borders::NONE));
+    let width = chunks[1].width.max(3) - 3;
+    let scroll = app.input.visual_scroll(width as usize);
+    let input = Paragraph::new(app.input.value())
+        .style(match app.mode {
+            InputMode::Normal => Style::default(),
+            InputMode::Editing => Style::default().fg(Color::Yellow),
+        })
+        .scroll((0, scroll as u16))
+        .block(Block::default().borders(Borders::NONE));
 
-        frame.render_widget(input, chunks_textinput[1]);
+    frame.render_widget(input, chunks[1]);
 
-        match app.mode {
-            InputMode::Normal => {}
-            InputMode::Editing => frame.set_cursor(
-                chunks_textinput[1].x + ((app.input.visual_cursor()).max(scroll) - scroll) as u16,
-                chunks_textinput[1].y,
-            ),
-        }
+    match app.mode {
+        InputMode::Normal => {}
+        InputMode::Editing => frame.set_cursor(
+            chunks[1].x + ((app.input.visual_cursor()).max(scroll) - scroll) as u16,
+            chunks[1].y,
+        ),
     }
+}
+
+pub fn render_main_area<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>, area: Rect) {
+    let chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Min(0), Constraint::Length(1)].as_ref())
+        .split(area);
+
+    let rows = app.items.iter().map(|show| {
+        Row::new(vec![
+            Cell::from(show.imdb_id.as_str()),
+            Cell::from(show.original_title.clone()),
+            Cell::from(show.release_year.unwrap().to_string()),
+        ])
+    });
 
     frame.render_stateful_widget(
         Table::new(rows)
@@ -99,7 +92,18 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
             .orientation(ScrollbarOrientation::VerticalRight)
             .begin_symbol(Some("↑"))
             .end_symbol(Some("↓")),
-        outer[1],
+        chunks[1],
         &mut app.scroll_state,
     )
+}
+
+/// Renders the user interface widgets.
+pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
+    let outer = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Length(1), Constraint::Min(0)].as_ref())
+        .split(frame.size());
+
+    render_input_area(app, frame, outer[0]);
+    render_main_area(app, frame, outer[1]);
 }
