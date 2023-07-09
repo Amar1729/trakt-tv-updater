971935e8920def1647a4735f2f23a98ab426edc9
Amar1729 <amar.paul16@gmail.com>
2023-07-09 16:37:53 -0400

    handler: ctrl-c from anywhere

--- src/interface/handler.rs
+++ src/interface/handler.rs
@@ -6,17 +6,21 @@ use tui_input::backend::crossterm::EventHandler;
 
 /// Handles the key events and updates the state of [`App`].
 pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
+    // Exit application from any mode on `Ctrl-C`
+    match key_event.code {
+        KeyCode::Char('c') | KeyCode::Char('C') => {
+            if key_event.modifiers == KeyModifiers::CONTROL {
+                app.quit();
+            }
+        }
+        _ => {}
+    }
+
     match app.mode {
         AppMode::MainView => match key_event.code {
             // Exit application on `ESC` or `q`
             KeyCode::Esc | KeyCode::Char('q') => {
                 app.quit();
-            }
-            // Exit application on `Ctrl-C`
-            KeyCode::Char('c') | KeyCode::Char('C') => {
-                if key_event.modifiers == KeyModifiers::CONTROL {
-                    app.quit();
-                }
             }
             KeyCode::Char('k') | KeyCode::Up => {
                 app.prev(1);
